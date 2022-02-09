ee3edb92d1de11d1cf135da49e5643317c985eb1
tluwaniMS <tluwani2020@gmail.com>
2022-02-09 08:18:35 +0200

    adding-database-population-catch-condition

--- main-migration-script.js
+++ main-migration-script.js
@@ -2,12 +2,20 @@ const { createSampleMoviesScript } = require("./migration-scripts/MovieMigration
 const { createSampleSuperHeroesScript } = require("./migration-scripts/SuperHeroMigrationScript");
 const { createSampleSuperPowersScript } = require("./migration-scripts/SuperPowerMigrationScript");
 const { connectToDataBase } = require("./database-coinfiguration");
+const { getAllMovies } = require("./database-queries/MoviesDBQueries");
 
-(async () => {
-  await connectToDataBase();
+const populateDatabase = async () => {
   await createSampleMoviesScript();
   await createSampleSuperHeroesScript();
   await createSampleSuperPowersScript();
+};
+
+(async () => {
+  await connectToDataBase();
+
+  const movies = await getAllMovies();
+
+  movies.length === 0 ? await populateDatabase() : (console.log(`Database has already been populated`), process.exit());
 })().then(() => {
   console.log(`Database population completed successfully...`);
   process.exit();

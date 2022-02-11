6afd5d6b9db4ace681947671109cef7e9bc1adcd
tluwaniMS <tluwani2020@gmail.com>
2022-02-11 09:42:00 +0200

    adding-error-handling-middleware

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const schema = require("./main-graphql-schema");
 const app = express();
 
 dotenv.config();
-app.use("/api/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
+app.use(
+  "/api/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    customFormatErrorFn: (error) => {
+      let message;
+      let statusCode;
+
+      console.log(error);
+
+      error.originalError
+        ? ((message = "There was an internal server error, please try again later"), (statusCode = 500))
+        : ((message =
+            "There was an error fulfilling your request, please check your request and ensure it's pointing to the correct end-point"),
+          (statusCode = 400));
+
+      return { message: message, statusCode: statusCode };
+    }
+  })
+);
 
 module.exports = { app };

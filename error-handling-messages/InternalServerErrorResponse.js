2b6ed6bfd89dca31a8ab2ab62ff02fb5fa07f357
tluwaniMS <tluwani2020@gmail.com>
2022-02-11 09:51:35 +0200

    adding-error-response-messages

---
+++ error-handling-messages/InternalServerErrorResponse.js
@@ -0,0 +1,4 @@
+const InternalServerErrorResponse = {
+  message: "There was an internal server error, please try again later",
+  statusCode: 500
+};
---
+++ error-handling-messages/UnknownRequestErrorResponse.js
@@ -0,0 +1,5 @@
+const UnknownRequestErrorResponse = {
+  message:
+    "There was an error fulfilling your request, please check your request and ensure it's pointing to the correct end-point",
+  statusCode: 500
+};
